d72f33c4b48abcc24b1140b2a444fa50b1944e0e
Steven Sousa <silver.steven@hotmail.com>
2026-01-04 14:56:18 -0500

    Overhaul of project using better-auth and prisma orm plus updated dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "clash-of-clans-s3.s3.us-east-2.amazonaws.com",
-        pathname: "/**",
-      },
-    ],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
